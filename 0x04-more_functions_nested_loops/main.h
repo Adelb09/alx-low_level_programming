49d82e9eaa40d7be5b4ab92c90b6137dff730f0c
Adelb09 <adelberhili99@gmail.com>
2023-03-23 09:30:27 -0700

    Add 5-more_numbers.c and main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -10,5 +10,19 @@ int _putchar(char c);
 void print_numbers(void);
 int _putchar(char c);
 void print_most_numbers(void);
+void _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_most_numbers(void);
+void more_numbers(void);
 
 #endif /* MAIN_H */
