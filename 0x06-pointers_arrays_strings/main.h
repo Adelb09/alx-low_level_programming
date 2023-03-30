f87c3b1e9a7ddc7d48b628f804ef01d2f4b14cd0
Adelb09 <adelberhili99@gmail.com>
2023-03-30 07:39:58 -0700

    Add solution for task 3

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,5 +5,17 @@ char *_strcat(char *dest, char *src);
 int _putchar(char c);
 char *_strncat(char *dest, char *src, int n);
 int _putchar(char c);
+char *_strncpy(char *dest, char *src, int n);
+int _putchar(char c);
+int _putchar(char c);
+int _strlen(char *s);
+void _puts(char *s);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+int _strcmp(char *s1, char *s2);
 
 #endif /* MAIN_H */
