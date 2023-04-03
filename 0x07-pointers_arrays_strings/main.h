f7f77bca8a4fc5e82f38858add156520510de7e9
Adelb09 <adelberhili99@gmail.com>
2023-04-03 10:55:22 -0700

    Add solution to 2-strchr task

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -11,4 +11,23 @@ void *_memset(void *s, int c, size_t n);
 
 char *_memcpy(char *dest, char *src, unsigned int n);
 
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
+/**
+ * _strchr - locates a character in a string
+ * @s: pointer to string
+ * @c: character to locate
+ *
+ * Return: pointer to the first occurrence of the character c in the string s,
+ * or NULL if the character is not found
+ */
+char *_strchr(char *s, char c);
+
 #endif /* MAIN_H */
