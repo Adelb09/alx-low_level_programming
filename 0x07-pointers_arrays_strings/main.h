6a17c532941f5020524b7a07adcfd0ae9af375a5
Adelb09 <adelberhili99@gmail.com>
2023-04-03 09:26:19 -0700

    your commit message here

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,4 +9,6 @@ int _putchar(char c);
 /* _memset - fills memory with a constant byte */
 void *_memset(void *s, int c, size_t n);
 
+char *_memcpy(char *dest, char *src, unsigned int n);
+
 #endif /* MAIN_H */
