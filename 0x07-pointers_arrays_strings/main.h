93d56695f610be5cee4b63a484782ed92daeba23
Adelb09 <adelberhili99@gmail.com>
2023-04-03 18:39:11 -0700

    Add solution for task 8

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -38,5 +38,6 @@ char *_strstr(char *haystack, char *needle);
 int _putchar(char c);
 void print_chessboard(char (*a)[8]);
 int _putchar(char c);
+void print_diagsums(int *a, int size);
 
 #endif /* MAIN_H */
