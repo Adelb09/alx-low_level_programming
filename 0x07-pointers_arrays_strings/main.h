45a6742059aad0f56109a0b57fae04535a13fa0d
Adelb09 <adelberhili99@gmail.com>
2023-04-03 18:34:03 -0700

    Add Chessboard Printer program

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -36,5 +36,7 @@ char *_strpbrk(char *s, char *accept);
 int _putchar(char c);
 char *_strstr(char *haystack, char *needle);
 int _putchar(char c);
+void print_chessboard(char (*a)[8]);
+int _putchar(char c);
 
 #endif /* MAIN_H */
