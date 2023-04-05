fbd87c9fbc8583db3bde18b5884c366c5f75b83e
Adelb09 <adelberhili99@gmail.com>
2023-04-05 06:49:51 -0700

    Add solution for task 0

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,5 +3,11 @@
 
 int _putchar(char c);
 void _puts_recursion(char *s);
+/* included libraries */
+#include <unistd.h>
+
+/* function prototypes */
+int _putchar(char c);
+void _print_rev_recursion(char *s);
 
 #endif /* MAIN_H */
