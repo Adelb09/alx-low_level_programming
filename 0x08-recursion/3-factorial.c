b94c1086f86bb988cea37d8461baca5a198af13f
Adelb09 <adelberhili99@gmail.com>
2023-04-05 18:26:05 -0700

    Add function to check if a number is prime

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * factorial - returns the factorial of a given number
- * @n: the number to compute the factorial of
+ * factorial - Calculates the factorial of a given number
+ * @n: The number to calculate the factorial of
  *
- * Return: the factorial of n, or -1 if n is negative
+ * Return: The factorial of the given number. If the number is less than 0,
+ *         the function should return -1 to indicate an error.
  */
 int factorial(int n)
 {
 if (n < 0)
-return -1;
-else if (n == 0)
-return 1;
+return (-1);
+else if (n == 0 || n == 1)
+return (1);
 else
-return n * factorial(n - 1);
+return (n * factorial(n - 1));
 }
