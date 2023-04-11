d8a048e0b968ab6c995c2ad14c7ee1a4a2392540
Adelb09 <adelberhili99@gmail.com>
2023-04-11 08:06:21 -0700

    Add solution to Task 4

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -22,4 +22,9 @@ int _putchar(char c);
 int _putchar(char c);
 int **alloc_grid(int width, int height);
 
+
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+int _putchar(char c);
+
 #endif /* MAIN_H */
