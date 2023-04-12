990fa89098ffec92d1cbdf9f9a8a89790317d776
Adelb09 <adelberhili99@gmail.com>
2023-04-12 07:13:10 -0700

    Add solution for task 100

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -13,5 +13,7 @@ int _putchar(char c);
 int is_positive_number(char *s);
 int string_to_integer(char *s);
 int add_numbers(int argc, char *argv[]);
+int _putchar(char c);
+int main(int argc, char **argv);
 
 #endif /* MAIN_H */
