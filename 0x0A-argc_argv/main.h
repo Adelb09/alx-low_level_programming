29604c354aea033ee7fa17f6f5ca688b9678ba84
Adelb09 <adelberhili99@gmail.com>
2023-04-12 07:07:28 -0700

    Add program that adds positive numbers

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -9,5 +9,9 @@ int _putchar(char c);
 int main(int argc, char **argv);  
 int main(int argc, char *argv[]);
 int _putchar(char c);
+int _putchar(char c);
+int is_positive_number(char *s);
+int string_to_integer(char *s);
+int add_numbers(int argc, char *argv[]);
 
 #endif /* MAIN_H */
