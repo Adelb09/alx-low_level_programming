7b37990d6143f73ee57dd37e83b603217a18476b
Adelb09 <adelberhili99@gmail.com>
2023-04-12 06:41:16 -0700

    Fix errors and warnings in 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Prints the number of arguments passed into it.
+ * @argc: The number of arguments passed to the program.
+ *
+ * Return: Always 0.
+ */
+int main(int argc)
+{
+printf("%d\n", argc - 1);
+return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -5,5 +5,7 @@
 
 int main(int argc, char *argv[]);
 int _putchar(char c);
+int _putchar(char c);
+int main(int argc, char **argv);  
 
 #endif /* MAIN_H */
