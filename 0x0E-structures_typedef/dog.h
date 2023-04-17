fde4cab561ff482d91f834ec9e2f34591f61aa7f
Adelb09 <adelberhili99@gmail.com>
2023-04-17 06:45:49 -0700

    Add struct dog definition and example usage

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure for a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: structure called dog with name, age, and owner elements
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif /* DOG_H */
